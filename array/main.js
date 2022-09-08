3852ed534a06a029bed5c13055a956fd8fabc27e
Manuel Ortiz <manuelortizd@gmail.com>
2022-09-08 16:44:56 -0300

    Primer Commit
    iniciando repositorio

---
+++ array/main.js
@@ -0,0 +1,6 @@
+const productos = ["Televisor", "Notebook", "PS5", "Xbox", "Parrilla" ]
+
+for (let i = 0; i <= 5; i++) {
+    console.log(productos[i])
+    
+}
---
+++ ciclos/main.js
@@ -0,0 +1,5 @@
+let number = prompt("Ingrese el numero para mostrar tabla hasta el 10")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i*number);    
+}
